14cac252ac943e56be3c1079c8ab851640d4e220
unknown <rachitkumarsingh144@gmail.com>
2026-01-17 14:06:14 +0530

    Simplify build script

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+echo "=== Starting build process ==="
+
 pip install -r requirements.txt
 
+echo "=== Running collectstatic ==="
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+echo "=== Running migrations ==="
+python manage.py migrate --run-syncdb
+
+echo "=== Build complete ==="
